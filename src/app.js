634a000aaca97d71afe7360f55db9e52caaa5391
cookRiceYu_admin <yuzheEmail@163.com>
2020-10-27 22:10:59 +0800

    save

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+const koaStatic = require('koa-static');
+// 监听错误信息
+onerror(app);
+
+/**
+ * 中间件
+ **/
+// 格式化请求参数
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text'],
+  })
+);
+app.use(json());
+// 日志
+app.use(logger());
+// 静态资源
+app.use(koaStatic(__dirname + '/public'));
+// ejs 文件渲染
+app.use(
+  views(__dirname + '/views', {
+    extension: 'ejs',
+  })
+);
+
+app.on('error', (err, ctx) => {
+  console.error('服务错误：', err, ctx);
+});
+
+module.exports = app;
